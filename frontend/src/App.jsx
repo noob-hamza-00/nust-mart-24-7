15b9c785293ede4dd55d7a53b09162a47bb7f3bb
noob-hamza-00 <mijaz.bee22seecs@seecs.edu.pk>
2025-09-17 02:11:39 +0500

    Change WhatsApp button to 'View Nust Mart' and update link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,10 +18,9 @@ export default function App() {
           <li key={item.id}>{item.name} <em>({item.category})</em></li>
         ))}
       </ul>
-      <p style={{color: 'gray', fontSize: 14}}>DAR K AGY BHOOT, APNY BUSINESS KI MAA KI CHOOOOOOOT...yayyyyyy</p>
-  {/* Info line removed as requested */}
+      <p style={{color: 'gray', fontSize: 14}}>This is a public, read-only list. To allow live adding/editing, a backend is required.</p>
       <a
-        href="https://wa.me/923169626873"
+        href="https://wa.me/c/923169626873"
         target="_blank"
         rel="noopener noreferrer"
         style={{
@@ -39,7 +38,7 @@ export default function App() {
         }}
       >
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" style={{marginRight: 8}}><path d="M12 2C6.477 2 2 6.477 2 12c0 1.85.504 3.58 1.38 5.07L2.05 21.95l4.88-1.33A9.953 9.953 0 0 0 12 22c5.523 0 10-4.477 10-10S17.523 2 12 2Zm0 18c-1.61 0-3.13-.39-4.45-1.08l-.32-.17-2.9.79.78-2.83-.2-.33A7.96 7.96 0 0 1 4 12c0-4.418 3.582-8 8-8s8 3.582 8 8-3.582 8-8 8Zm4.13-5.47c-.22-.11-1.3-.64-1.5-.71-.2-.07-.35-.11-.5.11-.15.22-.57.71-.7.86-.13.15-.26.17-.48.06-.22-.11-.93-.34-1.77-1.09-.66-.59-1.1-1.31-1.23-1.53-.13-.22-.01-.34.1-.45.1-.1.22-.26.33-.39.11-.13.15-.22.22-.37.07-.15.04-.28-.02-.39-.07-.11-.5-1.21-.68-1.66-.18-.44-.36-.38-.5-.39-.13-.01-.28-.01-.43-.01-.15 0-.39.06-.6.28-.21.22-.8.78-.8 1.9 0 1.12.82 2.2.94 2.35.12.15 1.61 2.46 3.91 3.36.55.24.98.38 1.31.49.55.18 1.05.15 1.44.09.44-.07 1.3-.53 1.48-1.04.18-.51.18-.95.13-1.04-.05-.09-.2-.15-.42-.26Z" fill="white"/></svg>
-        Contact on WhatsApp
+        View Nust Mart
       </a>
     </div>
   );
